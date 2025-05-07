fe9345a504fd83ddd585d39e64e12e21252dee08
Vedant Vohra <vohra.vedant@gmail.com>
2025-05-07 11:43:43 -0400

    Remove duplicate Add-Navbar-Link directory and unnecessary files

--- Add-Navbar-Link/site/entrepreneurship-without-address/src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from '@/components/LoginForm';
-
-export default function LoginPage() {
-  return (
-    <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded shadow">
-      <LoginForm />
-    </div>
-  );
-}
